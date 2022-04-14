15dda48ccb3f26e72b82618b6b2d71dbaec44ec0
hanmarjacobs <70344014+hanmarjacobs@users.noreply.github.com>
2022-04-14 15:05:12 -0600

    UPDATE - Basic Format and Leaderboard Integration
    - App Summary at the top
    - State variable to display Leaderboard

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import LeaderBoard from './Leaderboard.js';
 
+/*
+  ************************************************************************************************
+                                        WORK IN PROGRESS 
+  Welcome to Snake!
+  Currently being developed by Amara Tariq, Jerry Liu, and Hannah Jacobson
+
+  App.js acts as the controlling code behind which view is displayed: The Main Menu, How to Play,
+  Snake, or The Leaderboard. 
+
+  Currently, the Leaderboard is being displayed for demonstration purposes.
+
+  Testing:
+
+  ToDo List:
+    Add boolean values and button controls for displaying the various components and screens.
+    Styling!
+
+  ************************************************************************************************
+*/
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  //Change to false hide Leaderboard
+  const [leaderView, setLeaderView] = useState(true);
+
+  var snakeui = <Text> Major Failure </Text>
+
+  if(leaderView){
+    snakeui = <View style={styles.container}>
+                <LeaderBoard/>
+              </View>
+  }
+  else{
+    snakeui = <View style={styles.container}>
+                "Default Screen."
+              </View>
+  }
+
+
+
+  return (snakeui)
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
+
